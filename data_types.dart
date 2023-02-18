1828dab01fb2df6646746535814d75baa82d120b
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 16:44:11 +0900

    Lists * Collection if    * Collection의 변수 안에서 if문에 true, false에 따라 값을 넣을 수      있다...오호

--- data_types.dart
+++ data_types.dart
@@ -1,10 +1,17 @@
 void main() {
-  String name = 'deok';
-  bool alive = true;
-  int age = 28;
-  double money = 79.99;
+  // List<int> numbers = [1, 2, 3, 4, 5];
+  var numbers = [1, 2, 3, 4, 5];
+  numbers.add(6);
+  numbers.last;
 
-  // int, double의 부모
-  num x = 12;
-  x = 1.1;
+  // Collection if
+  var giveMeFive = true;
+  var numbers2 = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  print(numbers2);
 }
