cae3542469fb4a3ee0678ce5868cb766d3a6c025
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 17:12:12 +0900

    Sets

--- data_types.dart
+++ data_types.dart
@@ -1,25 +1,8 @@
 void main() {
-  // 컴파일러가 타입 추론으로 var 사용 가능
-  var player = {
-    'name': 'deok',
-    'xp': 19.99,
-    'superpower': false,
-  };
-
-  // 명시적 특정도 가능
-  Map<int, bool> test1 = {
-    1: true,
-    2: false,
-    3: true,
-  };
-
-  Map<List<int>, bool> test2 = {
-    [1, 2, 3, 4, 5]: true,
-  };
-
-  List<Map<String, Object>> players2 = [
-    {'hello': 'world'},
-    {'good': 123},
-  ];
-  print(players2);
+  var numbers = {1, 2, 3, 4};
+  Set<int> numbers2 = {1, 2, 3, 4, 5};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }
