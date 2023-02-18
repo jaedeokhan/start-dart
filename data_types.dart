c716e32c2691171b45b66d5a759c93f5df7a145c
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 17:01:16 +0900

    Collection For * 변수안에 Collection For를 이용해서 값을 추가 가능

--- data_types.dart
+++ data_types.dart
@@ -1,8 +1,12 @@
 void main() {
-  // 단순 변수를 기입 : $변수
-  // 연산 : ${변수 + 2}
-  var name = 'deok';
-  var age = 10;
-  var greeting = 'Hello, my name is $name, and I\'m ${age + 2}';
-  print(greeting);
+  // collection for
+  var oldFriends = ['deok', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "*$friend"
+  ];
+
+  print(newFriends);
 }
