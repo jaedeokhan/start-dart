2c57ce219c38cab9b59baabcfa68be54a85879cd
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 17:08:44 +0900

    Maps * var로 컴파일러 추론 방식 가능 * Map<int, bool> player; 직접 선언 방식 가능

--- data_types.dart
+++ data_types.dart
@@ -1,12 +1,25 @@
 void main() {
-  // collection for
-  var oldFriends = ['deok', 'lynn'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for (var friend in oldFriends) "*$friend"
-  ];
+  // 컴파일러가 타입 추론으로 var 사용 가능
+  var player = {
+    'name': 'deok',
+    'xp': 19.99,
+    'superpower': false,
+  };
 
-  print(newFriends);
+  // 명시적 특정도 가능
+  Map<int, bool> test1 = {
+    1: true,
+    2: false,
+    3: true,
+  };
+
+  Map<List<int>, bool> test2 = {
+    [1, 2, 3, 4, 5]: true,
+  };
+
+  List<Map<String, Object>> players2 = [
+    {'hello': 'world'},
+    {'good': 123},
+  ];
+  print(players2);
 }
