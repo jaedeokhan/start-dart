73621e5bdb3daef54b2f4e39fa4b0eab32de7b26
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 15:42:35 +0900

    Nullable Variables * Dart의 변수는 기본적으로 non-nullable이다. * nullable로 가능하게 하려면 String?처럼 변수 타입 앞에 물음표(?)를 붙인다. * if (deok != null)처럼 null 처리 후 함수를 사용하는 단축 표현은   deok?.isNotEmpty와 같이 변수 앞에 물음표를 붙이면 null이 아닌 처리를 단축 표현이 가능하다

--- main.dart
+++ main.dart
@@ -1,11 +1,12 @@
 void main() {
-  dynamic name;
-  if(name is String){
-    // 타입 검증을 하면 안에서 name의 String이나 자신의 타입 함수 사용 가능
+  String? deok = 'deok';
+  deok = null;
+
+  // null 회피 첫 번째 방법
+  if (deok != null) {
+    deok.isNotEmpty;
   }
 
-  if(name is int){
-  
-  }
-
+  // null 회피 단축 방법
+  deok?.isNotEmpty;
 }
