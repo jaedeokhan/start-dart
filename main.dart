dd25a1ff131f90b75552b80f2a8aec11da87bfbe
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 16:02:37 +0900

    Const Variables * js, ts에서 const와는 조금 다르다. * Dart에서 const는 compile-time-const이다. * 컴파일을 할 때 컴파일러가 알고있는 상수값을 말한다.

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 void main() {
-  // late는 초기 데이터없이 변수를 선언할 수 있게 한다.
-  // 값을 넣기전에는 데이터를 사용하면 에러가 난다.
-  // flutter API data fetching시 유용
-  late final String name;
-  // do something, go to api
-  name = 'deok';
-  print(name);
+  // compile-time constant
+  // const는 컴파일을 할 때 알고있는 상수값을 사용해야한다.
+  // 컴파일러가 알고 있는 값을 사용
+  // fetchApi() 함수로 API에서 key를 가져와야 할 때 서버에서 가져오는 값은
+  // 컴파일러가 알지 못한다. 그래서 하단 구문은 final을 사용
+  // const API = fetchApi();
+  final API = "121212";
+
+  // 앱스토어에 올리기 전에 알 수 있는 값
+  const max_allowed_price = 120;
 }
