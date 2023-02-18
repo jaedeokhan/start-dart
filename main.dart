c1bd23be061e99c2255b8eeb3a85017024d52a3d
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 15:48:47 +0900

    Final Variables

--- main.dart
+++ main.dart
@@ -1,12 +1,6 @@
 void main() {
-  String? deok = 'deok';
-  deok = null;
-
-  // null 회피 첫 번째 방법
-  if (deok != null) {
-    deok.isNotEmpty;
-  }
-
-  // null 회피 단축 방법
-  deok?.isNotEmpty;
+  // final만 작성해도 Dart 컴파일러가 String인지 유츄한다.
+  final name = 'deok';
+  // final String처럼 더 명시적으로 해줘도 상관없지만 위에 방식이 더 단순하다.
+  final String name2 = "deok";
 }
