8bfefc5a84a8f5a85b991a334ffdf1f02ab8104e
jaedeokhan <hjaedeok@gmail.com>
2023-02-18 15:32:14 +0900

    Dynamic Type * var name; 과 같이 var로 선언만 해도 마우스 호버를 하면 dynamic으로 설정 * dynamic name; 과 같이 dynamic으로 선언 가능 * 이상적으로 정말 필요할 때만 쓰는게 좋다

--- main.dart
+++ main.dart
@@ -1,7 +1,11 @@
 void main() {
-  var localVar = '니꼬';
-  localVar = 'nico';
+  dynamic name;
+  if(name is String){
+    // 타입 검증을 하면 안에서 name의 String이나 자신의 타입 함수 사용 가능
+  }
 
-  String classAndPropertyVar = '니꼬';
-  classAndPropertyVar = 'nico';
+  if(name is int){
+  
+  }
+
 }
