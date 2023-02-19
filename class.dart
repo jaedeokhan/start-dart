acbad4cfe9db849916c95950362d6be141b4b07a
jaedeokhan <hjaedeok@gmail.com>
2023-02-19 17:14:05 +0900

    Constructors * late를 사용해서 Java와 비슷한 방식으로 생성하는 방법 * late를 사용하지 않고, 생성자의 파라미터에 this.name, this.xp로 설정만   해서 간단하게 하는 방법

--- class.dart
+++ class.dart
@@ -1,6 +1,17 @@
 class Player {
-  final String name = 'deok';
-  int xp = 1500;
+  // late final String name;
+  // late int xp;
+
+  // Player(String name, int xp) {
+  //   this.name = name;
+  //   this.xp = xp;
+  // }
+
+  // late를 사용하지 않고 생성자를 더 간단하게 사용하는 방법
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
 
   void sayHello() {
     print("Hi my name is $name");
@@ -8,7 +19,8 @@ class Player {
 }
 
 void main() {
-  var player = Player();
-  print(player.name);
+  var player = Player('deok', 1500);
   player.sayHello();
+  var player2 = Player('duck', 2000);
+  player2.sayHello();
 }
