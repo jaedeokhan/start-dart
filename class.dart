11fb3f1f6770a72bc989034a0bad0c7f97a2c6b5
jaedeokhan <hjaedeok@gmail.com>
2023-02-19 17:42:32 +0900

    class Recap

--- class.dart
+++ class.dart
@@ -2,48 +2,38 @@ class Player {
   final String name;
   int xp;
   String team;
-  int age;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-
-  // Named Constructors 방법
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
-
-  // Position Constructors 방법
-  Player.createRedPlayer(
-    String name,
-    int age,
-  )   : this.age = age,
-        this.name = name,
-        this.team = 'red',
-        this.xp = 0;
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 
   void sayHello() {
-    print(
-        "Hi my name is $name, and I'm age $age, and my team color $team, my xp $xp");
+    print("Hi my name is $name, my team color $team, my xp $xp");
   }
 }
 
 void main() {
-  var bluePlayer = Player.createBluePlayer(
-    name: 'deok',
-    age: 12,
-  );
-  var redPlayer = Player.createRedPlayer(
-    'duck',
-    12,
-  );
-  bluePlayer.sayHello();
-  redPlayer.sayHello();
+  var apiData = [
+    {
+      "name": "deok",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "dal",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
